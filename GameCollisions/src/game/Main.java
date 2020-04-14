9698f5a7a852a293df3c887c494da66dea5a5e2e
pedro <pedro@DESKTOP-59Q8AVJ.home>
2020-04-14 20:17:22 +0100

    somos porto

--- GameCollisions/src/game/Main.java
+++ GameCollisions/src/game/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static final int WIDTH = 680;
 	public static final int HEIGHT = WIDTH / 16 * 9;
-	public static final float SCALE = 2.5f;
+	public static final float SCALE = 1.5f;
 	
 	public static final String TITLE = "Game";
 	
